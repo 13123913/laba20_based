a7f05813d20d6aaf9acb49ec20adb80711cd1417
13123913 <perelygin_0505@mail.ru>
2023-09-27 10:54:18 +0700

    feat: based

---
+++ WinFormsApp20/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace WinFormsApp20
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            newCtrlNToolStripMenuItem = new ToolStripMenuItem();
+            openF3ToolStripMenuItem = new ToolStripMenuItem();
+            saveF2ToolStripMenuItem = new ToolStripMenuItem();
+            hghgToolStripMenuItem = new ToolStripSeparator();
+            exitAltXToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            undoCtrlZToolStripMenuItem = new ToolStripMenuItem();
+            renoCtrlShiftZToolStripMenuItem = new ToolStripMenuItem();
+            penToolStripMenuItem = new ToolStripMenuItem();
+            styleToolStripMenuItem = new ToolStripMenuItem();
+            solidToolStripMenuItem = new ToolStripMenuItem();
+            dotToolStripMenuItem = new ToolStripMenuItem();
+            dashDotDotToolStripMenuItem = new ToolStripMenuItem();
+            colorToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            aboutF1ToolStripMenuItem = new ToolStripMenuItem();
+            errorProvider1 = new ErrorProvider(components);
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            toolStripButton2 = new ToolStripButton();
+            toolStripButton3 = new ToolStripButton();
+            toolStripButton4 = new ToolStripButton();
+            toolStripButton5 = new ToolStripSeparator();
+            toolStripButton6 = new ToolStripButton();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            panel2 = new Panel();
+            trackBar1 = new TrackBar();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newCtrlNToolStripMenuItem, openF3ToolStripMenuItem, saveF2ToolStripMenuItem, hghgToolStripMenuItem, exitAltXToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // newCtrlNToolStripMenuItem
+            // 
+            newCtrlNToolStripMenuItem.Image = (Image)resources.GetObject("newCtrlNToolStripMenuItem.Image");
+            newCtrlNToolStripMenuItem.Name = "newCtrlNToolStripMenuItem";
+            newCtrlNToolStripMenuItem.Size = new Size(141, 22);
+            newCtrlNToolStripMenuItem.Text = "New Ctrl+N";
+            // 
+            // openF3ToolStripMenuItem
+            // 
+            openF3ToolStripMenuItem.Image = (Image)resources.GetObject("openF3ToolStripMenuItem.Image");
+            openF3ToolStripMenuItem.Name = "openF3ToolStripMenuItem";
+            openF3ToolStripMenuItem.Size = new Size(141, 22);
+            openF3ToolStripMenuItem.Text = "Open    F3";
+            // 
+            // saveF2ToolStripMenuItem
+            // 
+            saveF2ToolStripMenuItem.Image = (Image)resources.GetObject("saveF2ToolStripMenuItem.Image");
+            saveF2ToolStripMenuItem.Name = "saveF2ToolStripMenuItem";
+            saveF2ToolStripMenuItem.Size = new Size(141, 22);
+            saveF2ToolStripMenuItem.Text = "Save      F2";
+            // 
+            // hghgToolStripMenuItem
+            // 
+            hghgToolStripMenuItem.Name = "hghgToolStripMenuItem";
+            hghgToolStripMenuItem.Size = new Size(138, 6);
+            // 
+            // exitAltXToolStripMenuItem
+            // 
+            exitAltXToolStripMenuItem.Image = (Image)resources.GetObject("exitAltXToolStripMenuItem.Image");
+            exitAltXToolStripMenuItem.Name = "exitAltXToolStripMenuItem";
+            exitAltXToolStripMenuItem.Size = new Size(141, 22);
+            exitAltXToolStripMenuItem.Text = "Exit      Alt+X";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { undoCtrlZToolStripMenuItem, renoCtrlShiftZToolStripMenuItem, penToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(39, 20);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // undoCtrlZToolStripMenuItem
+            // 
+            undoCtrlZToolStripMenuItem.Image = (Image)resources.GetObject("undoCtrlZToolStripMenuItem.Image");
+            undoCtrlZToolStripMenuItem.Name = "undoCtrlZToolStripMenuItem";
+            undoCtrlZToolStripMenuItem.Size = new Size(194, 22);
+            undoCtrlZToolStripMenuItem.Text = "Undo             Ctrl+Z";
+            // 
+            // renoCtrlShiftZToolStripMenuItem
+            // 
+            renoCtrlShiftZToolStripMenuItem.Image = (Image)resources.GetObject("renoCtrlShiftZToolStripMenuItem.Image");
+            renoCtrlShiftZToolStripMenuItem.Name = "renoCtrlShiftZToolStripMenuItem";
+            renoCtrlShiftZToolStripMenuItem.Size = new Size(194, 22);
+            renoCtrlShiftZToolStripMenuItem.Text = "Reno         Ctrl+Shift+Z";
+            // 
+            // penToolStripMenuItem
+            // 
+            penToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { styleToolStripMenuItem, colorToolStripMenuItem });
+            penToolStripMenuItem.Image = (Image)resources.GetObject("penToolStripMenuItem.Image");
+            penToolStripMenuItem.Name = "penToolStripMenuItem";
+            penToolStripMenuItem.Size = new Size(194, 22);
+            penToolStripMenuItem.Text = "Pen";
+            // 
+            // styleToolStripMenuItem
+            // 
+            styleToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { solidToolStripMenuItem, dotToolStripMenuItem, dashDotDotToolStripMenuItem });
+            styleToolStripMenuItem.Name = "styleToolStripMenuItem";
+            styleToolStripMenuItem.Size = new Size(142, 22);
+            styleToolStripMenuItem.Text = "             Style";
+            // 
+            // solidToolStripMenuItem
+            // 
+            solidToolStripMenuItem.Checked = true;
+            solidToolStripMenuItem.CheckOnClick = true;
+            solidToolStripMenuItem.CheckState = CheckState.Checked;
+            solidToolStripMenuItem.DoubleClickEnabled = true;
+            solidToolStripMenuItem.Name = "solidToolStripMenuItem";
+            solidToolStripMenuItem.Size = new Size(138, 22);
+            solidToolStripMenuItem.Text = "Solid";
+            // 
+            // dotToolStripMenuItem
+            // 
+            dotToolStripMenuItem.CheckOnClick = true;
+            dotToolStripMenuItem.DoubleClickEnabled = true;
+            dotToolStripMenuItem.Name = "dotToolStripMenuItem";
+            dotToolStripMenuItem.Size = new Size(138, 22);
+            dotToolStripMenuItem.Text = "Dot";
+            // 
+            // dashDotDotToolStripMenuItem
+            // 
+            dashDotDotToolStripMenuItem.CheckOnClick = true;
+            dashDotDotToolStripMenuItem.DoubleClickEnabled = true;
+            dashDotDotToolStripMenuItem.Name = "dashDotDotToolStripMenuItem";
+            dashDotDotToolStripMenuItem.Size = new Size(138, 22);
+            dashDotDotToolStripMenuItem.Text = "DashDotDot";
+            // 
+            // colorToolStripMenuItem
+            // 
+            colorToolStripMenuItem.Image = (Image)resources.GetObject("colorToolStripMenuItem.Image");
+            colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            colorToolStripMenuItem.Size = new Size(142, 22);
+            colorToolStripMenuItem.Text = "             Color";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutF1ToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutF1ToolStripMenuItem
+            // 
+            aboutF1ToolStripMenuItem.Image = (Image)resources.GetObject("aboutF1ToolStripMenuItem.Image");
+            aboutF1ToolStripMenuItem.Name = "aboutF1ToolStripMenuItem";
+            aboutF1ToolStripMenuItem.Size = new Size(146, 22);
+            aboutF1ToolStripMenuItem.Text = "About         F1";
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.Left;
+            toolStrip1.GripMargin = new Padding(4);
+            toolStrip1.ImageScalingSize = new Size(70, 50);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1, toolStripButton2, toolStripButton3, toolStripButton4, toolStripButton5, toolStripButton6 });
+            toolStrip1.Location = new Point(0, 24);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(75, 426);
+            toolStrip1.Stretch = true;
+            toolStrip1.TabIndex = 1;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(72, 54);
+            toolStripButton1.Text = "toolStripButton1";
+            toolStripButton1.Click += toolStripButton1_Click;
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(72, 54);
+            toolStripButton2.Text = "toolStripButton2";
+            toolStripButton2.Click += toolStripButton2_Click;
+            // 
+            // toolStripButton3
+            // 
+            toolStripButton3.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton3.Image = (Image)resources.GetObject("toolStripButton3.Image");
+            toolStripButton3.ImageTransparentColor = Color.Magenta;
+            toolStripButton3.Name = "toolStripButton3";
+            toolStripButton3.Size = new Size(72, 54);
+            toolStripButton3.Text = "toolStripButton3";
+            toolStripButton3.Click += toolStripButton3_Click;
+            // 
+            // toolStripButton4
+            // 
+            toolStripButton4.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton4.Image = (Image)resources.GetObject("toolStripButton4.Image");
+            toolStripButton4.ImageTransparentColor = Color.Magenta;
+            toolStripButton4.Name = "toolStripButton4";
+            toolStripButton4.Size = new Size(72, 54);
+            toolStripButton4.Text = "toolStripButton4";
+            // 
+            // toolStripButton5
+            // 
+            toolStripButton5.Name = "toolStripButton5";
+            toolStripButton5.Size = new Size(72, 6);
+            // 
+            // toolStripButton6
+            // 
+            toolStripButton6.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton6.Image = (Image)resources.GetObject("toolStripButton6.Image");
+            toolStripButton6.ImageTransparentColor = Color.Magenta;
+            toolStripButton6.Name = "toolStripButton6";
+            toolStripButton6.Size = new Size(72, 54);
+            toolStripButton6.Text = "toolStripButton6";
+            toolStripButton6.Click += toolStripButton6_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(108, 45);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(639, 375);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pictureBox2);
+            panel1.Location = new Point(98, 27);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(661, 393);
+            panel1.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(11, 8);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(638, 371);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(trackBar1);
+            panel2.Location = new Point(99, 425);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(660, 25);
+            panel2.TabIndex = 4;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(468, 1);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(174, 45);
+            trackBar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
+            Controls.Add(toolStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem newCtrlNToolStripMenuItem;
+        private ToolStripMenuItem openF3ToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem undoCtrlZToolStripMenuItem;
+        private ToolStripMenuItem renoCtrlShiftZToolStripMenuItem;
+        private ToolStripMenuItem penToolStripMenuItem;
+        private ToolStripMenuItem styleToolStripMenuItem;
+        private ToolStripMenuItem solidToolStripMenuItem;
+        private ToolStripMenuItem dotToolStripMenuItem;
+        private ToolStripMenuItem dashDotDotToolStripMenuItem;
+        private ToolStripMenuItem colorToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutF1ToolStripMenuItem;
+        private ErrorProvider errorProvider1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripButton toolStripButton2;
+        private ToolStripButton toolStripButton3;
+        private ToolStripMenuItem saveF2ToolStripMenuItem;
+        private ToolStripSeparator hghgToolStripMenuItem;
+        private ToolStripMenuItem exitAltXToolStripMenuItem;
+        private ToolStripButton toolStripButton4;
+        private ToolStripSeparator toolStripButton5;
+        private ToolStripButton toolStripButton6;
+        public ToolStrip toolStrip1;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private PictureBox pictureBox2;
+        private Panel panel2;
+        private TrackBar trackBar1;
+    }
+}
---
+++ WinFormsApp20/Form1.cs
@@ -0,0 +1,109 @@
+using System.Drawing.Imaging;
+using System.Net.NetworkInformation;
+
+namespace WinFormsApp20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                }
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveDlg = new SaveFileDialog();
+            SaveDlg.Filter = "JPEG Image|*.jpg|Bitmap Image|*.bmp|GIF Image|*.gif | PNG Image | *.png";
+            SaveDlg.Title = "Save an Image File";
+            SaveDlg.FilterIndex = 4; SaveDlg.ShowDialog();
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            Bitmap pic = new Bitmap(750, 500);
+            picDrawingSurface.Image = pic;
+            if (picDrawingSurface.Image == null)
+            {
+                MessageBox.Show("Сначала создайте новый файл");
+                return;
+            }
+            SaveFileDialog SaveDlg = new SaveFileDialog();
+            SaveDlg.Filter = "JPEG Image|*.jpg|Bitmap Image|*.bmp|GIF Image|*.gif|PNG Image|*.png";
+            SaveDlg.Title = "Save an Image File";
+            SaveDlg.FilterIndex = 4;
+            SaveDlg.ShowDialog();
+            if (SaveDlg.FileName != "")
+            {
+                System.ID.FileStream fs = (System.ID.FileStream)SaveDlg.OpenFile;
+
+                switch (SaveDlg.FilterIndex)
+                {
+                    case 1:
+                        this.picDrawingSurface.Image.Save(fs, ImageFormat.Jpeg);
+                        break;
+
+                    case 2:
+                        this.picDrawingSurface.Image.Save(fs, ImageFormat.Bmp);
+                        break;
+
+                    case 3:
+                        this.picDrawingSurface.Image.Save(fs, ImageFormat.Gif);
+                        break;
+                    case 4:
+                        this.picDrawingSurface.Image.Save(fs, ImageFormat.Png);
+                        break;
+                }
+
+                fs.Close();
+            }
+
+            if (picDrawingSurface.Image != null)
+            {
+                var result = MessageBox.Show("Сохранить текущее изображение перед созданием нового рисунка?", "Предупреждение", MessageBoxButtons.YesNoCancel);
+
+                switch (result)
+                {
+                    case DialogResult.No: break;
+                    case DialogResult.Yes: saveMenu_Click(sender, e); break;
+                    case DialogResult.Cancel: return;
+                }
+            }
+        }
+    }
+}
